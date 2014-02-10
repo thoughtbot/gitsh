2d77cc4a0bd32fab1d98a2195bb15a4ea880cf0d
George Brocklehurst <george@georgebrock.com>
2014-02-10 13:49:00 +0100

    Use the same git command everywhere.
    The `GitRepository` and `GitCommand` classes were using different git binaries
    when the `--git` option was used. This could cause minor inconsistencies,
    for example `hub` commands worked but could not be tab completed.
    
    Fixing this revealed a potential bug in the `History` class, when a
    non-numeric `gitsh.historySize` setting was used, so that's fixed too.

--- spec/units/git_repository_spec.rb
+++ spec/units/git_repository_spec.rb
@@ -6,13 +6,13 @@ describe Gitsh::GitRepository do
   describe '#initialized?' do
     it 'returns true when the current directory is a git repository' do
       Dir.chdir(repository_root) do
-        expect(Gitsh::GitRepository.new).to be_initialized
+        expect(Gitsh::GitRepository.new(env)).to be_initialized
       end
     end
 
     it 'returns false when the current directory is not a git repository' do
       Dir.chdir('/') do
-        expect(Gitsh::GitRepository.new).not_to be_initialized
+        expect(Gitsh::GitRepository.new(env)).not_to be_initialized
       end
     end
   end
@@ -20,7 +20,7 @@ describe Gitsh::GitRepository do
   describe '#current_head' do
     it 'returns the name of the current git branch' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         run 'git init'
         expect(repo.current_head).to eq 'master'
         run 'git checkout -b my-feature'
@@ -30,7 +30,7 @@ describe Gitsh::GitRepository do
 
     it 'returns the name of the current git branch with a forward slash' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         run 'git init'
         run 'git checkout -b feature/foo'
         expect(repo.current_head).to eq 'feature/foo'
@@ -39,7 +39,7 @@ describe Gitsh::GitRepository do
 
     it 'returns the name of an annotated tag if there is no branch' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         run 'git init'
         run 'git commit --allow-empty -m "First"'
         run 'git tag -m "Tag pointing to first" first'
@@ -52,7 +52,7 @@ describe Gitsh::GitRepository do
 
     it 'returns the an abbreviated SHA if there is no branch or tag' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         run 'git init'
         run 'git commit --allow-empty -m "First"'
         run 'git commit --allow-empty -m "Second"'
@@ -64,7 +64,7 @@ describe Gitsh::GitRepository do
 
     it 'returns nil in an uninitialized repository' do
       Dir.chdir('/') do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         expect(repo.current_head).to be_nil
       end
     end
@@ -73,7 +73,7 @@ describe Gitsh::GitRepository do
   context '#has_untracked_files?' do
     it 'returns true when there are untracked files in the repository' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         run 'git init'
         expect(repo).not_to have_untracked_files
         write_file 'example.txt'
@@ -85,7 +85,7 @@ describe Gitsh::GitRepository do
   context '#has_modified_files?' do
     it 'returns true when there are modified files' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         run 'git init'
         write_file 'example.txt'
         expect(repo).not_to have_modified_files
@@ -100,7 +100,7 @@ describe Gitsh::GitRepository do
   context '#heads' do
     it 'produces all the branches and tags' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         run 'git init'
         run 'git commit --allow-empty -m "Something swell"'
         expect(repo.heads).to eq %w( master )
@@ -113,7 +113,7 @@ describe Gitsh::GitRepository do
 
   context '#commands' do
     it 'produces the list of porcelain commands' do
-      repo = Gitsh::GitRepository.new
+      repo = Gitsh::GitRepository.new(env)
       expect(repo.commands).to include %(add)
       expect(repo.commands).to include %(commit)
       expect(repo.commands).to include %(checkout)
@@ -126,7 +126,7 @@ describe Gitsh::GitRepository do
   context '#aliases' do
     it 'produces the list of aliases' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         run 'git init'
         run 'git config --local alias.zecho "!echo zzz"'
         run 'git config --local aliasy.notanalias "not an alias"'
@@ -140,7 +140,7 @@ describe Gitsh::GitRepository do
   context '#config' do
     it 'returns a git configuration value' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         run 'git init'
         run 'git config --local alias.zecho "!echo zzz"'
         expect(repo.config('alias.zecho')).to eq '!echo zzz'
@@ -149,14 +149,14 @@ describe Gitsh::GitRepository do
 
     it 'returns nil if the configuration variable is not set' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         expect(repo.config('not-a.real-variable')).to be_nil
       end
     end
 
     it 'returns the default value if the configuration variable is not set' do
       in_a_temporary_directory do
-        repo = Gitsh::GitRepository.new
+        repo = Gitsh::GitRepository.new(env)
         expect(repo.config('not-a.real-variable', 'a-default')).to eq 'a-default'
       end
     end
@@ -168,5 +168,9 @@ describe Gitsh::GitRepository do
 
   def run(command)
     Open3.capture3(command)
+  end
+
+  def env
+    stub(git_command: '/usr/bin/env git')
   end
 end
