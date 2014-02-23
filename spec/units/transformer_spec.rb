a002065b63e28a68a4bcfab42ad041c35d3f62cb
Alex Sunderland <alex.sunderland@gmail.com>
2014-02-23 16:39:40 +0000

    Test empty string transformation as a unit test.

--- spec/units/transformer_spec.rb
+++ spec/units/transformer_spec.rb
@@ -18,6 +18,14 @@ describe Gitsh::Transformer do
       expect(output).to be_a Gitsh::GitCommand
     end
 
+    it 'transforms args wit h empty strings passed to them' do
+      output = transformer.apply(
+        { arg: '' },
+        env: env
+      )
+      expect(output).to eq ''
+    end
+
     it 'transforms internal commands' do
       output = transformer.apply({ internal_cmd: 'set' }, env: env)
       expect(output).to be_a Gitsh::InternalCommand::Set
