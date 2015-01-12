54172c610d90f4882704211ed1050e52d1cb2bb2
George Brocklehurst & Saron Yitbarek <george+saron@thoughtbot.com>
2015-01-12 13:08:05 -0500

    Break up dense spec.

--- spec/integration/prompt_spec.rb
+++ spec/integration/prompt_spec.rb
@@ -1,13 +1,28 @@
 require 'spec_helper'
 
 describe 'The gitsh prompt' do
-  it 'is customised by a git config variable' do
+  it 'defaults to the directory basename and the branch name' do
     GitshRunner.interactive do |gitsh|
       gitsh.type('init')
+
       expect(gitsh).to prompt_with "#{cwd_basename} master@ "
+    end
+  end
+
+  it 'can be customised with a Git config variable' do
+    GitshRunner.interactive do |gitsh|
+      gitsh.type('init')
       gitsh.type('config gitsh.prompt "on %b %#"')
+
       expect(gitsh).to prompt_with 'on master @ '
+    end
+  end
+
+  it 'can be customised with a gitsh environment variable' do
+    GitshRunner.interactive do |gitsh|
+      gitsh.type('init')
       gitsh.type(':set gitsh.prompt "%d:%b%#"')
+
       expect(gitsh).to prompt_with "#{Dir.getwd}:master@ "
     end
   end
