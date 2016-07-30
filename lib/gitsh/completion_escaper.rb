32e7bfdb7987b37be3864275f983be5c4ddde481
George Brocklehurst <george@georgebrock.com>
2016-07-30 14:37:32 -0400

    Refactor: simplify Gitsh::CompletionEscaper

--- lib/gitsh/completion_escaper.rb
+++ lib/gitsh/completion_escaper.rb
@@ -2,8 +2,8 @@ require 'gitsh/parser'
 
 module Gitsh
   class CompletionEscaper
-    UNQUOTED_STRING_ESCAPABLES = Gitsh::Parser::UNQUOTED_STRING_ESCAPABLES
-    QUOTED_STRING_ESCAPABLES = {
+    ESCAPABLES = {
+      nil => Gitsh::Parser::UNQUOTED_STRING_ESCAPABLES,
       '"' => Gitsh::Parser::SOFT_STRING_ESCAPABLES,
       "'" => Gitsh::Parser::HARD_STRING_ESCAPABLES,
     }.freeze
@@ -14,61 +14,33 @@ module Gitsh
     end
 
     def call(input)
-      OptionEscaper.new(
-        completer,
-        input,
-        line_editor,
-      ).escaped_options
+      completer.call(unescape(input)).map { |option| escape(option) }
     end
 
     private
 
     attr_reader :completer, :line_editor
 
-    class OptionEscaper
-      def initialize(completer, input, line_editor)
-        @completer = completer
-        @input = input
-        @line_editor = line_editor
+    def escape(option)
+      escaped = option.gsub(/([#{escapables}])(?!$)/) { |char| "\\#{char}" }
+
+      if completing_quoted_argument?
+        escaped.strip
+      else
+        escaped
       end
+    end
 
-      def escaped_options
-        unescaped_options.map { |option| escape(option) }
-      end
+    def unescape(input)
+      input.gsub(/\\([#{escapables}])/, '\1')
+    end
 
-      private
+    def escapables
+      @_escapables ||= ESCAPABLES[line_editor.completion_quote_character]
+    end
 
-      attr_reader :completer, :input, :line_editor
-
-      def unescaped_options
-        completer.call(unescape(input))
-      end
-
-      def escape(option)
-        escaped = option.gsub(/([#{escapables}])(?!$)/) { |char| "\\#{char}" }
-
-        if completing_quoted_argument?
-          escaped.strip
-        else
-          escaped
-        end
-      end
-
-      def unescape(input)
-        input.gsub(/\\([#{escapables}])/, '\1')
-      end
-
-      def escapables
-        if completing_quoted_argument?
-          QUOTED_STRING_ESCAPABLES[line_editor.completion_quote_character]
-        else
-          UNQUOTED_STRING_ESCAPABLES
-        end
-      end
-
-      def completing_quoted_argument?
-        !line_editor.completion_quote_character.nil?
-      end
+    def completing_quoted_argument?
+      !line_editor.completion_quote_character.nil?
     end
   end
 end
