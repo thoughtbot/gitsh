b82dce39c363bc80962a9cec67e9991c4a6621f0
George Brocklehurst <george@georgebrock.com>
2017-05-05 15:06:34 -0400

    Draft completion file, and ways to install/find it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'pry'
 
 $LOAD_PATH.unshift(File.expand_path('../../ext', __FILE__))
 
+GITSH_CONFIG_DIRECTORY = File.expand_path('../../etc', __FILE__)
+
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each do |path|
   require path
 end
