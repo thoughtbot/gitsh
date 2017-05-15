1ed14c7696b43448dcc520b85872b7b5da857713
Cedric Staniewski <cedric@gmx.ca>
2017-05-15 22:43:41 +0200

    Expand historyFile path
    Expand the configured history file path, so it is possible to use a "~"
    for the user's home directory instead of an absolute path.
    
    Before, a path like "~/.local/share/gitsh_history" led to the following
    error:
    
    gitsh: Error: No such file or directory @ rb_sysopen - ~/.local/share/gitsh_history
    
    (Test added by George Brocklehurst)

--- spec/integration/persistent_history_spec.rb
+++ spec/integration/persistent_history_spec.rb
@@ -12,4 +12,15 @@ describe 'Gitsh history' do
       expect(gitsh).to output_error(/'foobarbaz' is not a git command/)
     end
   end
+
+  it 'uses the gitsh.historyFile setting' do
+    with_a_temporary_home_directory do
+      settings = { 'gitsh.historyFile' => '~/my_gitsh_history' }
+      GitshRunner.interactive(settings: settings) do |gitsh|
+        gitsh.type('foobarbaz')
+      end
+
+      expect(File.read("#{ENV['HOME']}/my_gitsh_history")).to match(/foobarbaz/)
+    end
+  end
 end
