757c8badb24ed4204cea121fbddeab6d41557036
George Brocklehurst <george@georgebrock.com>
2019-04-08 09:02:31 -0400

    Add a GitRepository to the Registry.

--- spec/support/registry.rb
+++ spec/support/registry.rb
@@ -1,9 +1,10 @@
 require 'gitsh/environment'
+require 'gitsh/git_repository'
 require 'gitsh/registry'
 
 module Registry
   def register_env(attrs = {})
-    default_atts = {
+    default_attrs = {
       config_directory: File.expand_path('../../etc', __FILE__),
       git_command: fake_git_path,
       print: nil,
@@ -13,7 +14,16 @@ module Registry
     }
     Gitsh::Registry[:env] = instance_double(
       Gitsh::Environment,
-      default_atts.merge(attrs),
+      default_attrs.merge(attrs),
+    )
+  end
+
+  def register_repo(attrs = {})
+    default_attrs = {
+    }
+    Gitsh::Registry[:repo] = instance_double(
+      Gitsh::GitRepository,
+      default_attrs.merge(attrs),
     )
   end
 end
