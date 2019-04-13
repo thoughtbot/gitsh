281a987f2ab084afe7371a5f6d632288d44c92e3
George Brocklehurst <george@georgebrock.com>
2019-04-13 10:53:57 -0400

    Simplify Interpreter initializer.

--- spec/units/interpreter_spec.rb
+++ spec/units/interpreter_spec.rb
@@ -7,8 +7,8 @@ describe Gitsh::Interpreter do
     it 'reads, parses, and executes each command from the input strategy' do
       env = double
       command = double(:command, execute: nil)
-      parser = double(:parser, parse: command)
-      lexer = double('Lexer', lex: tokens([:WORD, 'commit']))
+      allow(Gitsh::Parser).to receive(:parse).and_return(command)
+      allow(Gitsh::Lexer).to receive(:lex).and_return(tokens([:WORD, 'commit']))
       input_strategy = double(:input_strategy, setup: nil, teardown: nil)
       allow(input_strategy).to receive(:read_command).and_return(
         'first command',
@@ -17,24 +17,21 @@ describe Gitsh::Interpreter do
       )
       interpreter = described_class.new(
         env: env,
-        parser: parser,
-        lexer: lexer,
         input_strategy: input_strategy,
       )
 
       interpreter.run
 
       expect(input_strategy).to have_received(:setup).ordered
-      expect(lexer).to have_received(:lex).with('first command').ordered
-      expect(lexer).to have_received(:lex).with('second command').ordered
+      expect(Gitsh::Lexer).to have_received(:lex).with('first command').ordered
+      expect(Gitsh::Lexer).to have_received(:lex).with('second command').ordered
       expect(input_strategy).to have_received(:teardown).ordered
       expect(command).to have_received(:execute).with(env).twice
     end
 
     it 'handles parse errors' do
       env = double(:env, puts_error: nil)
-      parser = double(:parser)
-      allow(parser).to receive(:parse).
+      allow(Gitsh::Parser).to receive(:parse).
         and_raise(RLTK::NotInLanguage.new([], double(:token), []))
       input_strategy = double(
         :input_strategy,
@@ -48,7 +45,6 @@ describe Gitsh::Interpreter do
       )
       interpreter = described_class.new(
         env: env,
-        parser: parser,
         input_strategy: input_strategy,
       )
 
@@ -61,11 +57,10 @@ describe Gitsh::Interpreter do
     it 'handles incomplete input by requesting a completion' do
       env = double
       command = double(:command, execute: nil)
-      parser = double(:parser, parse: command)
-      lexer = double('Lexer')
-      allow(lexer).to receive(:lex).with('(commit').
+      allow(Gitsh::Parser).to receive(:parse).and_return(command)
+      allow(Gitsh::Lexer).to receive(:lex).with('(commit').
         and_return(tokens([:LEFT_PAREN], [:WORD, 'commit'], [:INCOMPLETE, ')']))
-      allow(lexer).to receive(:lex).with("(commit\n)").
+      allow(Gitsh::Lexer).to receive(:lex).with("(commit\n)").
         and_return(tokens([:LEFT_PAREN], [:WORD, 'commit'], [:RIGHT_PAREN]))
       input_strategy = double(
         :input_strategy,
@@ -79,27 +74,24 @@ describe Gitsh::Interpreter do
       )
       interpreter = described_class.new(
         env: env,
-        parser: parser,
         input_strategy: input_strategy,
-        lexer: lexer,
       )
 
       interpreter.run
 
-      expect(lexer).to have_received(:lex).
+      expect(Gitsh::Lexer).to have_received(:lex).
         with('(commit').ordered
-      expect(lexer).to have_received(:lex).
+      expect(Gitsh::Lexer).to have_received(:lex).
         with("(commit\n)").ordered
-      expect(parser).to have_received(:parse).once
+      expect(Gitsh::Parser).to have_received(:parse).once
       expect(command).to have_received(:execute)
     end
 
     it 'drops the command if the completion is nil' do
       env = double
-      parser = double(:parser, parse: nil)
-      lexer = double(
-        'Lexer',
-        lex: tokens([:LEFT_PAREN], [:WORD, 'commit'], [:INCOMPLETE, ')']),
+      allow(Gitsh::Parser).to receive(:parse)
+      allow(Gitsh::Lexer).to receive(:lex).and_return(
+        tokens([:LEFT_PAREN], [:WORD, 'commit'], [:INCOMPLETE, ')']),
       )
       input_strategy = double(
         :input_strategy,
@@ -113,15 +105,13 @@ describe Gitsh::Interpreter do
       )
       interpreter = described_class.new(
         env: env,
-        parser: parser,
         input_strategy: input_strategy,
-        lexer: lexer,
       )
 
       interpreter.run
 
-      expect(lexer).to have_received(:lex).once
-      expect(parser).not_to have_received(:parse)
+      expect(Gitsh::Lexer).to have_received(:lex).once
+      expect(Gitsh::Parser).not_to have_received(:parse)
     end
   end
 end
